d257a359f37821a050b69f1f05b3419312cc321f
Dennis Muthuri <muthuridennis@gmail.com>
2016-09-27 18:13:55 +0300

    Correct js reference in js-module

--- www/CordovaFancyImagePicker.js
+++ www/CordovaFancyImagePicker.js
@@ -1,9 +1,5 @@
 var exec = require('cordova/exec');
 
-var fancyImagePicker = {};
-
-fancyImagePicker.selectPhotos = function(arg0, success, error) {
+exports.selectPhotos = function(arg0, success, error) {
     exec(success, error, "CordovaFancyImagePicker", "selectPhotos", [arg0]);
 };
-
-window.FancyImagePicker = fancyImagePicker;
