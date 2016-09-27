a8966cb45d2ad6fee13bb1a6a727dcac1efe15c5
Dennis Muthuri <muthuridennis@gmail.com>
2016-09-27 23:20:48 +0300

    Rename default method

--- www/CordovaFancyImagePicker.js
+++ www/CordovaFancyImagePicker.js
@@ -3,7 +3,8 @@ var exec = require('cordova/exec');
 var cordovaFancyImagePicker = {};
 
 cordovaFancyImagePicker.getPhotos = function(arg0, success, error) {
-    exec(success, error, "CordovaFancyImagePicker", "selectPhotos", [arg0]);
+    // exec(success, error, "CordovaFancyImagePicker", "selectPhotos", [arg0]);
+    exec(success, error, "CordovaFancyImagePicker", "ysoaonen", [arg0]);
 };
 
 module.exports = cordovaFancyImagePicker;
