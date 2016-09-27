23bf42dd97fdbf5e8511bf3067286d4a4d86ec26
Dennis Muthuri <muthuridennis@gmail.com>
2016-09-27 17:57:33 +0300

    add objects to the window

--- www/CordovaFancyImagePicker.js
+++ www/CordovaFancyImagePicker.js
@@ -6,5 +6,4 @@ fancyImagePicker.selectPhotos = function(arg0, success, error) {
     exec(success, error, "CordovaFancyImagePicker", "selectPhotos", [arg0]);
 };
 
-
-module.exports = fancyImagePicker;
+window.FancyImagePicker = fancyImagePicker;
