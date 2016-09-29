c2ce3f5e82344da48efc47afaa4a89fcee10f29e
Dennis Muthuri <muthuridennis@gmail.com>
2016-09-29 23:48:11 +0300

    Add a styles and colors files to match the ones created by the plugin

--- src/android/MultiImageSelect.java
+++ src/android/MultiImageSelect.java
@@ -19,7 +19,6 @@ import gun0912.tedbottompicker.TedBottomPicker;
 
 public class MultiImageSelect extends AppCompatActivity {
     final int INTENT_REQUEST_GET_IMAGES = 13;
-//    final int MAX_IMAGE_COUNT= 5;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,19 +26,8 @@ public class MultiImageSelect extends AppCompatActivity {
 
         Log.d("Images", "<------------ Loading images ------------>");
 
-        Intent intent  = new Intent(MultiImageSelect.this, ImagePickerActivity.class);
+        Intent intent  = new Intent(this, ImagePickerActivity.class);
         startActivityForResult(intent,INTENT_REQUEST_GET_IMAGES);
-//        TedBottomPicker tedBottomPicker = new TedBottomPicker.Builder(MultiImageSelect.this)
-//                .setMaxCount(MAX_IMAGE_COUNT)
-//                .setOnImageSelectedListener(new TedBottomPicker.OnImageSelectedListener() {
-//                    @Override
-//                    public void onImageSelected(Uri uri) {
-//                        Log.i("Images >>", "Uri ------------> " + uri);
-//                    }
-//                })
-//                .create();
-//
-//        tedBottomPicker.show(getSupportFragmentManager());
     }
 
     @Override
@@ -55,25 +43,4 @@ public class MultiImageSelect extends AppCompatActivity {
             Log.i("Images >>", "These images are not there");
         }
     }
-    // public void sayYes(View view) {
-    //     Intent data = new Intent();
-    //     Bundle res = new Bundle();
-    //     res.putString("YES", "I say Yes");
-    //     data.putExtras(res);
-    //     setResult(RESULT_OK, data);
-    //     finish();
-    // }
-
-    // public void sayNo(View view) {
-    //     Intent data = new Intent();
-    //     Bundle res = new Bundle();
-    //     res.putString("NO", "I say NO");
-    //     data.putExtras(res);
-    //     setResult(RESULT_CANCELED, data);
-    //     finish();
-    // }
-
-    // public void sayMaybe(View view) {
-    //     finish();
-    // }
 }
